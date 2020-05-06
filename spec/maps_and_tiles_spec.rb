f0428e381609e09eabfb01d7162bf755938b14e8
Neil Slater <neil@aframe.com>
2020-05-06 20:26:48 +0100

    Remove templated spec

--- spec/maps_and_tiles_spec.rb
+++ spec/maps_and_tiles_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe MapsAndTiles do
   it "has a version number" do
     expect(MapsAndTiles::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
